e06e0f650b51f931141b815bd9cf570f9bec485c
Tomas Vondra <tv@fuzzy.cz>
2016-08-16 21:13:19 +0200

    fix error in count_distinct as window function regression test
    Incorrect syntax in the SQL script (extra ROW) and copy'n'pasted
    output of the regression test.

--- test/sql/window.sql
+++ test/sql/window.sql
@@ -8,7 +8,7 @@ BEGIN;
 select count_distinct(a) over (order by a rows between unbounded preceding and current row)
   from generate_series(1, 20) a;
 
-select count_distinct(a) over (order by a rows between 10 preceding and 10 following row)
+select count_distinct(a) over (order by a rows between 10 preceding and 10 following)
   from generate_series(1, 25) a;
 
 ROLLBACK;
