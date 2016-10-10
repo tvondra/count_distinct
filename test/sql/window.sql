a5165654a39b52dd487dfc1ffe25d2dafdd623b5
Alexey Bashtanov <alexey@brandwatch.com>
2016-10-10 10:56:44 +0100

    stable branch for old versions of postgres (8.4+)

--- test/sql/window.sql
+++ test/sql/window.sql
@@ -1,13 +1,14 @@
 \set ECHO none
 BEGIN;
 
-\i sql/count_distinct--1.3.2.sql
+\i sql/count_distinct--1.3.3.sql
 
 \set ECHO all
 
 select count_distinct(a) over (order by a rows between unbounded preceding and current row)
   from generate_series(1, 20) a;
 
+-- This test will fail on postgres 8.4, as this syntax is not supported
 select count_distinct(a) over (order by a rows between 10 preceding and 10 following)
   from generate_series(1, 25) a;
 
